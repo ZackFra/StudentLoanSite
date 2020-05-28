c40814f7c285ded3e42b46314b228def6d02af58
ZackFra <zack11frazier22@gmail.com>
2020-05-28 08:16:11 -0400

    added site login

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
+const LoanData = require('./models/LoanData.model');
 
 // @route   POST login request
 // @desc    always fails
 // @access  public
 router.post('/', (req, res) => {
     return res.status(500).json(false);
+})
+
+// @route   POST login request
+// @desc    returns user data
+// @access  public
+router.post('/ClientLogin', (req, res) => {
+    const {user, pass} = req.body;
+    LoanData.findOne({user, pass})
+    .then( data => {
+        return res.status(200).json(data);
+    })
+    .catch( err => {
+        return res.status(500).json(false);
+    })
 })
 
 module.exports = router;
